775d1e781fc57eac2fab365389fa006440b9631e
Marco Ghilardelli <marco.ghilardelli@students.fhnw.ch>
2018-03-14 17:31:41 +0100

    Initial commit

---
+++ app/src/main/java/ch/fhnw/edu/emoba/sphero/PairingActivity.java
@@ -0,0 +1,13 @@
+package ch.fhnw.edu.emoba.sphero;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PairingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pairing);
+    }
+}
