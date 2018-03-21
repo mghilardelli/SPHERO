d466fb2367347566e012afdb9079cc3e887a6e18
Marco Ghilardelli <marco.ghilardelli@students.fhnw.ch>
2018-03-21 15:27:06 +0100

    try drawing a circle

--- app/src/main/java/ch/fhnw/edu/emoba/sphero/TabTouch.java
+++ app/src/main/java/ch/fhnw/edu/emoba/sphero/TabTouch.java
@@ -1,7 +1,11 @@
 package ch.fhnw.edu.emoba.sphero;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +15,19 @@ import android.view.ViewGroup;
  */
 
 public class TabTouch extends Fragment {
+
+    private Paint circlePaint;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_tab_touch, container, false);
+
+        circlePaint = new Paint();
+        circlePaint.setStyle(Paint.Style.FILL);
+        circlePaint.setAntiAlias(true);
+        circlePaint.setColor(Color.WHITE);
         return rootView;
     }
 }
