a4ddbf9274c8ccc7ce736a868243c395f384c1da
Marco Ghilardelli <marco.ghilardelli@students.fhnw.ch>
2018-03-21 14:26:32 +0100

    PairingActivity with Tab View

---
+++ app/src/main/java/ch/fhnw/edu/emoba/sphero/TabSensor.java
@@ -0,0 +1,20 @@
+package ch.fhnw.edu.emoba.sphero;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by marcoghilardelli on 21.03.18.
+ */
+
+public class TabSensor extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_tab_sensor, container, false);
+        return rootView;
+    }
+}
